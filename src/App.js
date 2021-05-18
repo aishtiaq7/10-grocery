0d9f4ea65ba26a52337d615bbf753118a6ab0c16
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2021-05-18 20:24:37 +0600

    onChange for input works

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
-  const [name,setName] = useState('');
-  const [list,setList] = useState([]);
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
-  const [editID,setEditID] = useState(null);
-  const [alert,setALert] = useState({show:false,msg:'',type:''});
+  const [editID, setEditID] = useState(null);
+  const [alert, setALert] = useState({ show: false, msg: "", type: "" });
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('clicked');
-  }
+    console.log("clicked");
+    // console.log(e.target);
+    console.log(name)
+    // console.log(e.target.value);
+  };
 
-  return <section className='section-center'>
-    <form className='grocery-form' onSubmit={handleSubmit}>
-      {alert.show && <Alert/>}
-      <h3>Grocery Bud</h3>
-      <div className='form-control'>
-        <input type='text'></input>
-        <button type='submit' className='submit-btn'>
-          {isEditing? 'Edit':'Submit'}
-        </button>
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>Grocery Bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="eg:eggs"
+            value={name}
+            onChange={(e) =>{setName(e.target.value)}}
+          ></input>
+          <button type="submit" className="submit-btn">
+            {isEditing ? "Edit" : "Submit"}
+          </button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">Clear Items</button>
       </div>
-    </form>
-    <div className='grocery-container'>
-      <List/>
-      <button className='clear-btn'>Clear Items</button>
-    </div>
-  </section>
+    </section>
+  );
 }
 
-export default App
+export default App;
